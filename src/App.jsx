ff5fd749e61efe9cb989baf58f61b52bcb31f245
Bilal <mbilalvisions2@gmail.com>
2025-09-19 08:28:24 -0700

    install flowbite

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 export default function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/adout" element={<About />} />
-          <Route path='/sign-in' element={<SignIn/>}/>
-          <Route path='/sign-up' element={<SignUp/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='/projects' element={<Projects/>}/>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navbar } from "flowbite-react";
+export default function Header() {
+  return (
+    <div>
+      <Navbar>Header</Navbar>
+    </div>
+  );
+}
